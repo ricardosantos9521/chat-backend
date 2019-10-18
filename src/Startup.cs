8d3562ef83bf7d73ac55ea8abec26b98dc211b9b
Ricardo Santos <ricardo.santos9521@outlook.com>
2019-10-18 15:40:23 +0100

    use hsts and HttpsRedirection in production

--- src/Startup.cs
+++ src/Startup.cs
@@ -83,6 +83,12 @@ namespace Chat.Backend
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+                app.UseHttpsRedirection();
+            }
 
             app.UseCors(builder =>
             {
